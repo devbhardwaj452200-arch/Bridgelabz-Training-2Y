5f0249a775227365c73749d1686cc54054395052
Dev Bhardwaj <devbhardwaj452200@gmail.com>
2026-08-05 09:31:13 +0530

    [Leetcode]

---
+++ Java_DSA/src/Arrays/Leetcode/Problem189.java
@@ -0,0 +1,37 @@
+package Arrays.Leetcode;
+
+import java.util.Arrays;
+
+public class Problem189 {
+
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem189 solution = new Problem189();
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        solution.rotate(nums, k);
+
+        System.out.println(Arrays.toString(nums));
+    }
+}
